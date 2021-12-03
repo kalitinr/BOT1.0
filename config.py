3ae86da9bbd0570399c5c01f44d1f8e1294732d4
kalitinr <95479465+kalitinr@users.noreply.github.com>
2021-12-03 17:42:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-TOKEN = '5023426840:AAGoQqPYmsHuDhlR7PxCncAOKtUDUgwxkj8'
+TOKEN = ''
 
 STARTMSG = "Добрый день это переводчик языков коренных народов РФ, для смены языка наберите /choose"
 KEYMSG = "Choose input or output - Выберите языки перевода"
