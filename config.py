cfb5e5a0c107787d631e1e3c49c7aa1df84eea67
kalitinr <95479465+kalitinr@users.noreply.github.com>
2021-12-03 17:29:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import sqlite3
 
 TOKEN = '5023426840:AAGoQqPYmsHuDhlR7PxCncAOKtUDUgwxkj8'
 
-STARTMSG = "Hi, i am translator bot. I use google python and google API. To change lang use /choose"
-KEYMSG = "Choose input or output"
+STARTMSG = "Добрый день это переводчик языков коренных народов РФ, для смены языка наберите /choose"
+KEYMSG = "Choose input or output - Выберите языки перевода"
 OUTMSG = "Choose output language"
 INMSG = "Choose input language"
 mydb = sqlite3.connect("base.sqlite")
